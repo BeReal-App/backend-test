fc03a540e99f782f3cff05053c3960113f814aa8
ntibi <ntibi@protonmail.com>
2021-12-03 15:39:57 +0100

    removed all references to postTime

--- src/content/post.dto.ts
+++ src/content/post.dto.ts
@@ -3,7 +3,6 @@ import { Media } from './media.dto';
 export class Post {
   uuid!: string;
   owner?: string;
-  postTime?: Date;
   caption?: string;
   frontPic?: Media;
   backPic?: Media;
--- src/discovery/post.loader.ts
+++ src/discovery/post.loader.ts
@@ -2,12 +2,10 @@ import { DiscoveryService } from './discovery.service';
 import { v4 as uuid } from 'uuid';
 
 export function loadPost(discoveryService: DiscoveryService, nb: number) {
-  const yesterday = Date.now() - 24 * 60 * 60 * 1000;
   for (let i = 0; i < nb; i++) {
     discoveryService.insert({
       uuid: uuid(),
       owner: uuid(),
-      postTime: new Date(yesterday + Math.random() * 24 * 60 * 60 * 1000),
     });
   }
 }
